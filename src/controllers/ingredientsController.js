d6e01dd0b47f51b547981ef1b54405b682147d21
Tomkiv_Ihor <igortomkiw3@gmail.com>
2026-06-12 21:00:22 +0300

    Implement ingredients management: add models, services, controllers, and routes

---
+++ src/controllers/ingredientsController.js
@@ -0,0 +1,7 @@
+import { getAllIngredients } from '../services/ingredients.js';
+
+getAllIngredients;
+
+export const getIngredients = async (req, res) => {
+  const {} = req.query;
+};
--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,10 +1,11 @@
-import { Router } from "express";
-import { celebrate } from "celebrate";
-import { authenticate } from "../middleware/authenticate.js";
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
+import { getIngredientsSchema } from '../validations/recipesValidation.js';
+import { getIngredients } from '../controllers/recipesController.js';
 
 const router = Router();
 
-
-
+router.get('/api/ingredients', celebrate(getIngredientsSchema), getIngredients); // path for a list of all ingredients
 
 export default router;
