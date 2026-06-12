fd8bb2d76a00c1558b3f18d1d96dda47fd2989bd
Andrii Khomenko <akhomenko1995@gmail.com>
2026-06-12 05:47:20 +0200

    Current User Information

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,20 @@
-import { Router } from "express";
-import { authenticate } from "../middleware/authenticate.js";
-import { upload } from "../middleware/multer.js";
-import { updateUserAvatar } from "../controllers/userController.js";
+import { Router } from 'express';
+import { authenticate } from '../middleware/authenticate.js';
+import { upload } from '../middleware/multer.js';
+import {
+  getCurrentUser,
+  updateUserAvatar,
+} from '../controllers/userController.js';
 
 const router = Router();
 
-router.patch('/users/me/avatar', authenticate, upload.single("avatar"), updateUserAvatar);
+router.patch(
+  '/users/me/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
+
+router.get('/users/me/', authenticate, getCurrentUser);
 
 export default router;
