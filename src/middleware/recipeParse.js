3bd8482703ca371629237a71e2f1f4f16a072317
MarinSenzzy <gwstudiogw48090@gmail.com>
2026-06-18 18:55:08 +0200

    rewrite middleware parseValidIngredients

--- src/middleware/recipeParse.js
+++
@@ -1,7 +0,0 @@
-export const parseIngredients = (req, res, next) => {
-  if (req.body.ingredients) {
-    req.body.ingredients = JSON.parse(req.body.ingredients);
-  }
-
-  next();
-};
--- src/validations/recipesValidation.js
+++ src/validations/recipesValidation.js
@@ -28,6 +28,35 @@ export const recipeIdSchema = {
   }),
 };
 
+const parseAndValidateIngredients = (value, helpers) => {
+  let parsedValue;
+
+  try {
+    parsedValue = typeof value === 'string' ? JSON.parse(value) : value;
+  } catch (err) {
+    return helpers.message('"ingredients" must be a valid JSON string');
+  }
+
+  const arraySchema = Joi.array()
+    .items(
+      Joi.object({
+        id: Joi.string().required(),
+        measure: Joi.string().required(),
+      }),
+    )
+    .min(2)
+    .max(16)
+    .required();
+
+  const { error } = arraySchema.validate(parsedValue);
+
+  if (error) {
+    return helpers.message(error.message);
+  }
+
+  return parsedValue;
+};
+
 export const createRecipeSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().max(64).required(),
@@ -35,16 +64,7 @@ export const createRecipeSchema = {
     time: Joi.number().min(1).max(360).required(),
     calories: Joi.number().integer().min(1).max(10000).optional(),
     category: Joi.string().required(),
-    ingredients: Joi.array()
-      .items(
-        Joi.object({
-          id: Joi.string().required(),
-          measure: Joi.string().required(),
-        }),
-      )
-      .min(2)
-      .max(16)
-      .required(),
+    ingredients: Joi.any().custom(parseAndValidateIngredients).required(),
     instructions: Joi.string().max(1200).required(),
   }),
 };
